e4e304bf748f8402a3974033e8066a7002c5694c
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-20 07:01:42 +0530

    add server create

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('testing');
+import { app }  from './app.js'
+import dotenv from 'dotenv'
+
+dotenv.config({path: './.env'})
+
+
+const PORT  = process.env.PORT || 8000
+app.listen(PORT, console.log(`Server is runing on port: ${PORT}`)
+)
+
