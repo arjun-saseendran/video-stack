da16c0a8cc2ef5b6a485a4ad40c6e474d7564a16
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-20 16:14:44 +0530

    add access token

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken'
 
 const userSchema = new Schema(
   {
@@ -52,6 +53,16 @@ userSchema.pre("save", async function (next) {
 
 userSchema.methods.isPasswordCorrect = async function(password){
   return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+  //short lived access token
+  return jwt.sign({
+    _id: this._id,
+    email: this.email,
+    username: this.username,
+    fullname: this.fullname
+  }, process.env.ACCESS_TOKEN_SERECT, {expiresIn: process.env.ACCESS_TOKEN_EXPIRY})
 }
 
 export const User = mongoose.model("User", userSchema);
