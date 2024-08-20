e0df49c6b0834c9623c482d895ab73b8a1aa4254
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-20 15:36:26 +0530

    add bcrypt

--- src/models/comment.models.js
+++ src/models/comment.models.js
@@ -1,41 +1,23 @@
 import mongoose, { Schema } from "mongoose";
-import MongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+import MongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-
-const commentSchema = new Schema({
+const commentSchema = new Schema(
+  {
     content: {
-        type: String,
-        required: true,
-        
-      },
-      video: {
-        type: Schema.Types.ObjectId,
-        ref: 'Video'
-      },
-      owner: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
-      },
-},
-{ timestamps: true }
-)
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
 
-videoSchema.plugin(MongooseAggregatePaginate)
+videoSchema.plugin(MongooseAggregatePaginate);
 export const Comment = mongoose.model("Comment", commentSchema);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/models/video.models.js
+++ src/models/video.models.js
@@ -1,5 +1,5 @@
 import mongoose, { Schema } from "mongoose";
-import MongooseAggregatePaginate from 'mongoose-aggregate-paginate-v2'
+import MongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new Schema(
   {
@@ -41,6 +41,6 @@ const videoSchema = new Schema(
   { timestamps: true }
 );
 
-videoSchema.plugin(MongooseAggregatePaginate)
+videoSchema.plugin(MongooseAggregatePaginate);
 
 export const Video = mongoose.model("Video", videoSchema);
