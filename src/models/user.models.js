bfda68ee0481a36ae2edbcb953e5418928538dc0
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-20 14:33:19 +0530

    add videoSchema

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,53 +1,47 @@
 import mongoose, { Schema } from "mongoose";
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    lowecase: true,
-    trim: true,
-    index: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowecase: true,
-    trim: true,
-    trim: true,
-    index: true,
-  },
-  avatar:{
-    type: String, //cloudinary URL
-    required: true,
-   },
-   coverImage:{
-    type: String, //cloudinary URL
-    
-   },
-   
-    watchHistory:[
-{
-    type: Schema.Types.ObjectId,
-    ref: "Videos"
-}
-
-
-    ],
-    password:{
-        type: String,
-        required: [true, 'password is required']
-
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+      index: true,
     },
-    refreshToken:{
-        type: String
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowecase: true,
+      trim: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary URL
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary URL
+    },
 
-    }
-   
-},
-{timestamps: true}
-
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Videos",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
 );
 
 export const User = mongoose.model("User", userSchema);
