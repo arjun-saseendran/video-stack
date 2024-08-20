610e2f24480226a8f0944526c1603913032e65df
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-20 15:46:23 +0530

    add bcrypt password

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -43,5 +44,10 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+  this.password = bcrypt.hash(this.password, 10);
+});
 
 export const User = mongoose.model("User", userSchema);
